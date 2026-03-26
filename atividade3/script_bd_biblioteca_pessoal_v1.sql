135f16daa3f12cac324a799c9252966051296d2a
lzz061 <luizaco654@gmail.com>
2026-03-25 21:21:56 -0300

    ATIVIDADE_3

--- atividade3/script_bd_biblioteca_pessoal_v1.sql
+++ atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -7,8 +7,9 @@ CREATE TABLE usuario(
 	id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(128) NOT NULL UNIQUE,
+    senha VARCHAR(128) NOT NULL,
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_atualização TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    data_atualização TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 /*3.Criando tabela categoria*/
@@ -17,70 +18,66 @@ CREATE TABLE categoria (
     nome VARCHAR(128) UNIQUE NOT NULL,
     descricao TEXT,
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_atualização TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    data_atualização TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 /*4.Criando tabela autor*/
 CREATE TABLE autor (
 	id_autor INT AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(128) NOT NULL,
+	nome VARCHAR(128) NOT NULL UNIQUE,
     ano_nascimento YEAR,
+    ano_morte YEAR,
     apresentacao TEXT,
-    data_cadastro TIMESTAMP,
-    data_atualizacao TIMESTAMP,
-    ano_morte YEAR
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-/*5.Criando tabela licro*/
-CREATE TABLE livro(
-	id_livro INT AUTO_INCREMENT PRIMARY KEY,
-	id_usuario INT NOT NULL,
-    id_autor INT NOT NULL,
-    id_editora INT NOT NULL,
-    id_categoria INT NOT NULL,
-    titulo VARCHAR(255) NOT NULL,
-    sinopse TEXT NOT NULL,
-    ano_publicacao YEAR NOT NULL,
-    lido BOOLEAN NOT NULL,
-    data_cadastro TIMESTAMP NOT NULL,
-    data_atualizacao TIMESTAMP
-);
-
-/*7. Tabela editoria*/
+/*5. Tabela editoria*/
 CREATE TABLE editora (
 	id_editora INT AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(255) UNIQUE,
     cidade VARCHAR(64) NOT NULL,
     estado VARCHAR(64) NOT NULL,
 	pais VARCHAR(64) NOT NULL,
-    data_cadastro TIMESTAMP NOT NULL,
-    data_atualizacao TIMESTAMP NOT NULL
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-/*6.Criando relacionamentos(USUARIO)*/
-ALTER TABLE livro 
-ADD CONSTRAINT fk_usuario 
+/*6.Criando tabela licro*/
+CREATE TABLE livro(
+	id_livro INT AUTO_INCREMENT PRIMARY KEY,
+	id_usuario INT ,
+    id_autor INT,
+    id_editora INT,
+    id_categoria INT,
+    titulo VARCHAR(255) NOT NULL,
+    sinopse TEXT,
+    ano_publicacao YEAR,
+    lido BOOLEAN DEFAULT (0),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+/*6.Criando relacionamentos(USUARIO_LIVRO)*/
+CONSTRAINT fk_usuario_livro 
 FOREIGN KEY (id_usuario) 
 REFERENCES usuario(id_usuario) 
-ON DELETE CASCADE;
+ON DELETE CASCADE,
 
-/*6.1 Criando relacionamentos(AUTOR)*/
-ALTER TABLE livro
-ADD CONSTRAINT fk_autor
+/*6.1 Criando relacionamentos(AUTOR_LIVRO)*/
+CONSTRAINT fk_autor_livro
 FOREIGN KEY (id_autor)
 REFERENCES autor(id_autor)
-ON DELETE RESTRICT;
+ON DELETE RESTRICT,
 
-/*6.2 Criando relacionamentos(EDITORA)*/
-ALTER TABLE livro
-ADD CONSTRAINT fk_editora
+/*6.2 Criando relacionamentos(EDITORA_LIVRO)*/
+CONSTRAINT fk_editora_livro
 FOREIGN KEY (id_editora)
 REFERENCES  editora(id_editora)
-ON DELETE RESTRICT;
+ON DELETE RESTRICT,
 
-/*6.3 Criando relacionamentos(CATEGORIA)*/
-ALTER TABLE livro
-ADD CONSTRAINT fk_categoria
+/*6.3 Criando relacionamentos(CATEGORIA_LIVRO)*/
+CONSTRAINT fk_categoria_livro
 FOREIGN KEY (id_categoria)
 REFERENCES categoria(id_categoria)
-ON DELETE RESTRICT;
+ON DELETE RESTRICT
+);
